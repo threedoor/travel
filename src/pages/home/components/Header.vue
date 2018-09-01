782cb419edac689a0ab4f0f89769f7a61cb88255
threedoor <you@example.com>
2018-09-01 16:45:38 +0800

    代码优化

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,25 +1,36 @@
 <template>
 <div class='header'>
-    <div class='header-left'>返回</div>
-    <div class='header-input'>输入城市景点</div>
-    <div class='header-right'>城市</div>
+    <div class='header-left'>
+        <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class='header-input'>
+        <span class="iconfont">&#xe632;</span>
+        输入城市景点</div>
+    <div class='header-right'>
+        城市 <span class="iconfont">&#xe6aa;</span>
+        </div>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'HomeHeader'
 }
 </script>
 
 <style lang='stylus' scoped>
+ @import "~style/varibles.styl"
 .header {
     display: flex;
     line-height: 0.86 rem;
-    background: #00bcd4;
+    background: $bgColor;
     color: #fff;
 
     .header-left {
+        .back-icon{
+            text-align: center;
+            font-size: 0.4 rem;
+        }
         width: 0.64 rem;
         float: left;
     }
@@ -30,8 +41,10 @@ export default {
         line-height: 0.64 rem;
         margin-top: 0.12 rem;
         margin-left: 0.2 rem;
+        padding-left: 0.2 rem;
         background: #fff;
         border-radius: 0.1 rem;
+        color:#ccc;
     }
 
     .header-right {
