d96bf81f3eb898180f324561c9e0d8c37ff1b0e3
threedoor <you@example.com>
2018-09-01 17:59:01 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,21 +1,22 @@
 <template>
 <div>
  <home-header></home-header>
+ <home-swpier></home-swpier>
 </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwpier from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwpier
   }
 }
 </script>
 
 <style>
-.home {
-  font-size: 60px;
-}
+
 </style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img
+                    class="swiper-img"
+                    :src="item.imgUrl"
+                    />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'https://img.mukewang.com/5b7407b5000159ba09360316.jpg'
+      },
+      {
+        id: '002',
+        imgUrl: 'https://img.mukewang.com/5b4c50e1000136fc18720632.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+.wrapper{
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    paddind-bottom: 31.25%;
+    background: #eee;
+    .swiper-img{
+        width: 100%;
+    }
+}
+</style>
